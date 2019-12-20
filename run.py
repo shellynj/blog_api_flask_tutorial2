897b97eb138191adb384e9fc1d358fde466ead7c
shellynj <mb884@njit.edu>
2019-12-19 22:32:33 -0500

    Task:Updated environment's port

--- run.py
+++ run.py
@@ -10,7 +10,8 @@ env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
 
 if __name__ == '__main__':
-  port = os.getenv('PORT')
+  #port = os.getenv('PORT')
+  port = 5000
   # run app
   app.run(host='127.0.0.1', port=port)
 
